0c16dcc88ef64048f428c5a5e8930c2a59420e34
Axel SERRIE <axel.serrie@gmail.com>
2020-01-13 14:51:24 +0100

    Modifs simple

--- src/components/musique-list-elem.js
+++ src/components/musique-list-elem.js
@@ -6,16 +6,13 @@ const MusiqueListItem = (props) =>{
         <li className="list-group-item">
             <div className="media">
                 <div className="media-left">
-                    <img className="media-object img-rounded img-VideoListItem" src={`${musique.albu}`} alt={movie.title}></img>
+                    <img className="media-object img-rounded img-VideoListItem" src={`${musique}`} alt={musique.title}></img>
                 </div>
             <div className="media-body">
-                <h5 className="title-list-item">{movie.title}</h5>
+                <h5 className="title-list-item">{musique.title}</h5>
             </div>
             </div>
         </li>
     )
-    function handleOnClick(){
-        props.callback(movie);
-    }
 }
 export default MusiqueListItem;
