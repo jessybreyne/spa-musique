b2e9bc4bc8eab527508aba2f4430e05208e10038
Jessy <jessybreyne@protonmail.com>
2020-01-16 00:06:58 +0100

    Component musique plus propre

--- src/components/musique.js
+++ src/components/musique.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const Musique = ({MUSIQUE_LINK}) => 
+const Musique = ({MUSIQUE_LINK,title,date}) => 
     (
-        <div>
+      <div className="card">
           <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src={"https://w.soundcloud.com/player/?url="+`${MUSIQUE_LINK}`+"&auto_play=true&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false"}></iframe>
-        </div>
+          <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{date}</p>
+              <a href="#" className="btn btn-primary">Voir</a>
+          </div>
+      </div>
     )
 
 export default Musique;
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react'
 import Musique from '../components/musique'
 import axios from 'axios'
-import MusiqueDetail from '../components/musique-detail'
 
 const LINK_LOCALHOST = "http://127.0.0.1:8000/"
 const MUSIQUES = "api/musiques.json"
@@ -27,46 +26,11 @@ class App extends Component{
 
     render(){
         return (
-
-                <div className="row">
-                    <div className="col-4">
-                        <div className="card">
-                            <Musique MUSIQUE_LINK={this.state.currentMusique.link}/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <MusiqueDetail title={this.state.currentMusique.titre} date={this.state.currentMusique.date} />
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div className="col-4">
-                        <div className="card">
-                            <Musique MUSIQUE_LINK={this.state.currentMusique.link}/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <MusiqueDetail title={this.state.currentMusique.titre} date={this.state.currentMusique.date} />
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-4">
-                        <div className="card">
-                            <Musique MUSIQUE_LINK={this.state.currentMusique.link}/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <MusiqueDetail title={this.state.currentMusique.titre} date={this.state.currentMusique.date} />
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-
+            <div className="row">
+                <div className="col-4">
+                    <Musique title={this.state.currentMusique.titre} date={this.state.currentMusique.date} MUSIQUE_LINK={this.state.currentMusique.link}/>  
                 </div>
-    
+            </div>
         )
     }
 }
